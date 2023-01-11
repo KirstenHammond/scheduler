0a4c6e7fd89b7f6fd9883ec2d299b22bf9341c01
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2023-01-10 18:12:31 -0800

    add daylist and update app.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,10 @@
 import React from "react";
-import classNames from "classnames";
+import classnames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
   //importing classNames instead of using conditional below
-  const buttonClass = classNames("button", {
+  const buttonClass = classnames("button", {
     "button--confirm": props.confirm,
     "button--danger": props.danger
   });

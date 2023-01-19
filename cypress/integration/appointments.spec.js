c17ebe0b02d96f7f7ff0102a88f08caaba6ec4c7
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2023-01-18 21:18:51 -0800

    write cypress tests and config suite

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,15 +1,15 @@
-describe("Appointment tests", () => {
+//Cypress end to end testing for Appointment / index.js
+//Accessing scheduler_test mock API database
 
+describe("Appointment tests", () => {
   beforeEach(() => {
-    cy.request("GET", "/api/debug/reset")
+    cy.request("GET", "/api/debug/reset");
     cy.visit("/");
     cy.contains("Monday");
   });
 
-  it("should book an interview", () => {
-    cy.get("[alt=Add]")
-      .first()
-      .click();
+  it("1. Should book an interview", () => {
+    cy.get("[alt=Add]").first().click();
 
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
     cy.get("[alt='Sylvia Palmer']").click();
@@ -18,35 +18,30 @@ describe("Appointment tests", () => {
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
-  })
+  });
 
-  it("should edit an interview", () => {
-    cy.get("[alt=Edit")
-      .first()
-      .click({ force: true })
+  it("2. Should edit an interview", () => {
+    cy.get("[alt=Edit").first().click({ force: true }); //Only visible on hover
+
     cy.get("[alt='Tori Malcolm']").click();
-    cy.get("[data-testid=student-name-input").clear().type("Lydia Miller-Jones");
+    cy.get("[data-testid=student-name-input")
+      .clear()
+      .type("Lydia Miller-Jones");
+
     cy.contains("Save").click();
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 
-  })
-
-  it("should cancel an interview", () => {
-    cy.get("[alt=Delete")
-      .first()
-      .click({ force: true })
+  it("3. Should cancel an interview", () => {
+    cy.get("[alt=Delete").first().click({ force: true });
 
     cy.contains("Confirm").click();
 
     cy.contains("Deleting").should("exist");
     cy.contains("Deleting").should("not.exist");
 
-    cy.contains(".appointment__card--show", "Archie Cohen")
-      .should("not.exist");
-
-  })
-
-
-})
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
+  });
+});
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,14 +1,15 @@
-describe("Navigation", () => {
-  it("should visit root", () => {
-    cy.visit("/");
+//Cypress end to end testing for DayList/ Daylist item
+//Accessing scheduler_test mock API database
 
+describe("Navigation", () => {
+  it("1. Should visit root", () => {
+    cy.visit("/");
   });
 
-  it("should navigate to Tuesday", () => {
+  it("2. Should navigate to Tuesday", () => {
     cy.visit("/");
     cy.contains("[data-testid=day]", "Tuesday")
-    .click()
-    .should("have.class", "day-list__item--selected");
-
+      .click()
+      .should("have.class", "day-list__item--selected");
   });
 });
